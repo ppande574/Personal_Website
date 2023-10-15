3911f35ced6cf678f6e6b916550ae12a9764c256
ppande574 <ppande0221@gmail.com>
2023-10-15 01:00:46 -0500

    Created initial navigation bar

--- personal-website/src/App.vue
+++ personal-website/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <NavigationBar></NavigationBar>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from "./components/NavigationBar.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar
   }
 }
 </script>
@@ -20,7 +21,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0d5298;
   margin-top: 60px;
 }
+
+#placeholder {
+  color: red; 
+}
+
 </style>
---
+++ personal-website/src/components/NavigationBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="navigation-bar">
+    <a class="active" href="#home">Home</a>
+    <a href="#experience">Experience</a>
+    <a href="#interests">Interests</a>
+  </div>
+</template>
+
+<script>
+    export default {
+    
+    };
+</script>
+
+<style>
+    /* Styling for navigation bar itself */
+    .navigation-bar {
+        margin: 0; 
+        padding: 0;
+        background-color: #333;
+        overflow: hidden;
+    }
+
+    /* Styling for links within nav bar */
+    .navigation-bar a {
+        float: left;
+        color: #ffffff;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    /* Hover over links */
+    .navigation-bar a:hover {
+        background-color: #ddd;
+        color: rgb(255, 1, 1);
+    }
+
+    /* Active link */
+    .navigation-bar a.active {
+        background-color: #04AA6D;
+        color: rgb(249, 249, 249);
+    }
+</style>
