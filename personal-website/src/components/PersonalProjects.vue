a8f007b7d18573927204dda2b312b53aefb67c6b
ppande574 <ppande0221@gmail.com>
2024-02-10 23:28:39 -0600

    Removed unnecessary contact page

--- personal-website/src/components/PersonalProjects.vue
+++ personal-website/src/components/PersonalProjects.vue
@@ -1,5 +1,6 @@
 <template>
-  <h1>Professional Software Development Project</h1>
+  <h1>Check out all of my projects on Github!</h1>
+  <h2>Professional Software Development Project</h2>
   <p>
     Spearheaded development of a robust API service for a restaurant b2b client
     using NestJS alongisde a team of student software engineers. Seamlessly
@@ -11,28 +12,27 @@
     database-backed web-application with product reports table to allow users to
     filter, search, and view information about individual products and
     inventory, as well as track pricing history and trends over time. Leveraged
-    VueJS, NestJS, NodeJS, PostgreSQL and TypeScript.
+    <span>VueJS, NestJS, NodeJS, PostgreSQL and TypeScript</span>.
   </p>
-  <h1>Currency Converter Console Application</h1>
+  <h2>Currency Converter Console Application</h2>
   <p>
-    Created a console-based currency converter in Java. Allows users to enter
+    Created a console-based currency converter in <span>Java</span>. Allows users to enter
     any two currencies, and the value they would like to convert as a decimal
     number. It then returns the converted value.
   </p>
-  <h1>Interactive Pong Game</h1>
+  <h2>Interactive Pong Game</h2>
   <p>
-    Created interactive, GUI-based, two-player game "Pong" game. Leveraged
+    Created interactive, GUI-based, two-player game "Pong" game in <span>Python</span>. Leveraged
     Python's PyGame library to handle user inputs, event-handling, and logic for
     dynamically updating screen.
   </p>
-  <h1>IN PROGRESS: Workout Application</h1>
+  <h2>IN PROGRESS: Workout Application</h2>
   <p>
-    A project utilizing the MERN tech-stack (MongoDB, Express, React, and
-    NodeJS) to help track workouts.
+    A project utilizing the <span>MongoDB, ReactJS, Express, and NodeJS</span> to help track workouts.
   </p>
-  <h1>IN PROGRESS: Currency Converter 2.0</h1>
+  <h2>IN PROGRESS: Currency Converter 2.0</h2>
   <p>
-    Leveraging C# (ASP.NET Web API) and ReactJS to build an updated, web-based
+    Leveraging <span>C# (ASP.NET Web API) and ReactJS</span> to build an updated, web-based
     Currency Converter.
   </p>
 </template>
@@ -42,5 +42,17 @@ p {
   text-align: left;
   padding-left: 25%;
   padding-right: 25%;
+}
+
+p span {
+  color: #4caf50
+}
+
+h1 {
+  color: #4caf50
+}
+
+h2 {
+  color: #f03a47
 }
 </style>
