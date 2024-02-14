297489e3f22499e1e3bc22291f1f931c84321e50
ppande574 <ppande0221@gmail.com>
2024-02-13 19:59:47 -0600

    Updated deployment files

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './components/HomePage.vue'
 import Contact from './components/ContactPage.vue'
 import Projects from './components/PersonalProjects.vue'
@@ -12,7 +12,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
